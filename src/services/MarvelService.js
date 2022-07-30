42d9245db548e07ee7306ae4275c336810229a2d
Aleksei <katakuxiko@gmail.com>
2022-07-30 18:48:47 +0600

    first commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,35 @@
+
+
+class MarvelService{
+	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	_apiKey = 'apikey=ff5ce3ecbcc8c665529b8eaa817b386d'
+	getResources = async (url)=>{
+		let res = await fetch(url);
+
+		if(!res.ok){
+			throw new Error(`Could not fetch ${url}`)
+		}
+		return await res.json();
+	}
+
+	getAllCharacters =async ()=>{
+		const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+		return res.data.results.map(this._transformCharacter);
+	}
+	getCharacter = async(id)=>{
+		const res = await this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+		return this._transformCharacter(res.data.results[0]);
+	}
+
+	_transformCharacter = (char)=>{
+		return {
+			  	name: char.name,
+                description: char.description,
+                thumbnail: char.thumbnail.path +'.'+ char.thumbnail.extension,
+                homepage: char.urls[0].url,
+                wiki: char.urls[1].url
+		}
+	}
+}
+
+export default MarvelService
